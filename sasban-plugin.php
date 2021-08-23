6fc24b54769f2ebf0618e59f790d868f483506e4
sasban <azure.saswata@gmail.com>
2021-08-23 14:02:57 +0530

    first commit

---
+++ sasban-plugin.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package SasbanPlugin
+ */
+
+/*
+Plugin Name: Sasban Plugin
+Plugin URI: https://sasban.plugin
+Description: This is my mega custom plugin from a tutorial series.
+Version: 1.0.0
+Author: Saswata "Sasban" Banerjee
+Author URI: https://styched.in
+License: GPLv2 or later
+Text Domain: sasban-plugin 
+*/
