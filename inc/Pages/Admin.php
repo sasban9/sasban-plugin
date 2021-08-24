86bd81faf0c9f92e62929f487f487638970ac017
sasban <azure.saswata@gmail.com>
2021-08-24 16:28:10 +0530

    Modular Admin Page.

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -6,23 +6,46 @@
 namespace Inc\Pages;
 
 use \Inc\Base\BaseController;
+use \Inc\Api\SettingsApi;
 
 class Admin extends BaseController
 {
+    public $settings;
+
+    public $pages = array();
+
+    public function __construct() {
+        $this->settings = new SettingsApi();
+
+        $this->pages = array(
+            array(
+                'page_title' => 'Sasban Plugin', 
+                'menu_title' => 'Sasban', 
+                'capability' => 'manage_options', 
+                'menu_slug' => 'ext_plugin', 
+                'callback' => function() { echo '<h1>Sasban Plugin (from callback)</h1>'; }, 
+                'icon_url' => 'dashicons-airplane', 
+                'position' => 110
+            ),
+            array(
+                'page_title' => 'Text Plugin', 
+                'menu_title' => 'Text Sasban', 
+                'capability' => 'manage_options', 
+                'menu_slug' => 'sasban_plugin', 
+                'callback' => function() { echo '<h1>TEXT Plugin (from cb)</h1>'; }, 
+                'icon_url' => 'dashicons-external', 
+                'position' => 15
+            ),
+        );
+    }
 
     public function register() {
         # add_action( 'wp_enqueue_scripts', array( $this, 'enqueue' ) );
 
-        add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+        // add_action( 'admin_menu', array( $this, 'add_admin_pages' ) );
+
+        $this->settings->addPages($this->pages)->register();
 
         // add_filter( "plugin_action_links_$this->plugin", array( $this, 'settings_link' ) );
-    }
-
-    public function add_admin_pages() {
-        add_menu_page( 'Sasban Plugin', 'Sasban', 'manage_options', 'sasban_plugin', array( $this, 'admin_index' ), 'dashicons-airplane', 110 );
-    }
-
-    public function admin_index() {
-        require_once $this->plugin_path . 'templates/admin.php';
     }
 }
