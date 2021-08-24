a733040b7f71212b4189798bef5d582b55cb0729
sasban <azure.saswata@gmail.com>
2021-08-24 18:33:12 +0530

    Admin Custom Fields - I

--- inc/Api/SettingsApi.php
+++ inc/Api/SettingsApi.php
@@ -11,6 +11,10 @@ class SettingsApi
 
     public $admin_subpages = array();
 
+    public $settings = array();
+    public $sections = array();
+    public $fields = array();
+
     public function register() 
     {
         if( !empty( $this->admin_pages ) ) {
@@ -64,6 +68,42 @@ class SettingsApi
 
         foreach ($this->admin_subpages as $page) {
             add_submenu_page($page['parent_slug'], $page['page_title'], $page['menu_title'], $page['capability'], $page['menu_slug'], $page['callback']);
+        }
+    }
+
+    public function setSettings( array $settings ) 
+    {
+        $this->settings = $settings;
+
+        return $this;
+    }
+
+    public function setSections( array $secfieldstions ) 
+    {
+        $this->sections = $sections;
+
+        return $this;
+    }
+
+    public function setFields( array $fields ) 
+    {
+        $this->fields = $fields;
+
+        return $this;
+    }
+
+    public function registerCustomFields()
+    {
+        foreach ($this->settings as $setting) {
+            register_setting($setting['option_group'], $setting['option_name'], (isset($setting['callback']) ? $setting['callback'] : ''));
+        }
+
+        foreach ($this->sections as $section) {
+            add_settings_section($section['id'], $section['title'], (isset($section['callback']) ? $section['callback'] : ''), $section['page']);
+        }
+
+        foreach ($this->fields as $field) {
+            add_settings_field($field['id'], $field['title'], (isset($field['callback']) ? $field['callback'] : ''), $field['page'], $field['section'], (isset($field['args']) ? $field['args'] : ''));
         }
     }
 }
