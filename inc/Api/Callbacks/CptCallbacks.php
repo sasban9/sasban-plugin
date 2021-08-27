8646a12df08bf84beb3af8d6341d8f7328b83b29
sasban <azure.saswata@gmail.com>
2021-08-27 14:40:48 +0530

    Editing a Custom Post Type.

--- inc/Api/Callbacks/CptCallbacks.php
+++ inc/Api/Callbacks/CptCallbacks.php
@@ -44,9 +44,15 @@ class CptCallbacks
         $name = $args['label_for'];
         $option_name = $args['option_name'];
         $placeholder = $args['placeholder'];
-        $input = get_option($option_name);
+        // $input = get_option($option_name);
+        $value = '';
 
-        echo '<input type="text" class="regular-text" id="'.$name.'" name="'.$option_name.'['.$name.']" value="" placeholder="'.$placeholder.'" required>';
+        if(isset($_POST['edit_post'])) {
+            $input = get_option($option_name);
+            $value = $input[$_POST['edit_post']][$name];
+        }
+
+        echo '<input type="text" class="regular-text" id="'.$name.'" name="'.$option_name.'['.$name.']" value="'.$value.'" placeholder="'.$placeholder.'" required>';
     }
 
     public function checkboxField($args)
@@ -55,8 +61,13 @@ class CptCallbacks
         $name = $args['label_for'];
         $classes = $args['classes'];
         $checkbox = get_option($option_name);
+        $checked = false;
 
-        $checked = isset($checkbox[$name]) ? ($checkbox[$name] ? true : false) : false;
+        if(isset($_POST['edit_post'])) {
+            $checkbox = get_option($option_name);
+            $checked = isset($checkbox[$_POST['edit_post']][$name]) ?: false;
+        }
+
         echo '<div class="'.$classes.'"><input type="checkbox" id="'.$name.'" name="'.$option_name.'['.$name.']" value="1" '.($checked?'checked':'').' ><label for="'.$name.'"><div></div></label></div>';
     }
 }
--- templates/cpt.php
+++ templates/cpt.php
@@ -1,15 +1,16 @@
 <div class="wrap">
     <h1 class="wp-heading-inline">CPT Manager</h1>
     <?php settings_errors(); ?>
+    <?php $active = isset($_POST['edit_post']) ? 'active' : ''; ?>
 
     <ul class="nav nav-tabs">
-        <li class="active"><a href="#tab-1">Your Custom Post Types</a></li>
-        <li><a href="#tab-2">Add Custom Post Type</a></li>
+        <li class="<?php echo str_replace($active, '', 'active'); ?>"><a href="#tab-1">Your Custom Post Types</a></li>
+        <li class="<?php echo $active; ?>"><a href="#tab-2"><?php echo isset($_POST['edit_post']) ? 'Edit' : 'Add'; ?> Custom Post Type</a></li>
         <li><a href="#tab-3">Export</a></li>
     </ul>
 
     <div class="tab-content">
-        <div id="tab-1" class="tab-pane active">
+        <div id="tab-1" class="tab-pane <?php echo str_replace($active, '', 'active'); ?>">
             <h3>Manage Your Custom Post Type</h3>
 
             <?php
@@ -22,14 +23,19 @@
 					$public = isset($option['public']) ? "TRUE" : "FALSE";
 					$archive = isset($option['has_archive']) ? "TRUE" : "FALSE";
 
-					echo "<tr><td>{$option['post_type']}</td><td>{$option['singular_name']}</td><td>{$option['plural_name']}</td><td class=\"text-center\">{$public}</td><td class=\"text-center\">{$archive}</td><td class=\"text-center\"><a href=\"#\">EDIT</a> - ";
+					echo "<tr><td>{$option['post_type']}</td><td>{$option['singular_name']}</td><td>{$option['plural_name']}</td><td class=\"text-center\">{$public}</td><td class=\"text-center\">{$archive}</td><td class=\"text-center\">";
+
+                    echo '<form method="POST" action="" class="inline-block">';
+                    echo '<input type="hidden" name="edit_post" value="'.$option['post_type'].'">';
+                    submit_button('Edit', 'primary small', 'submit', false);
+                    echo '</form> ';
+
 
                     echo '<form method="POST" action="options.php" class="inline-block">';
                     settings_fields('sasban_plugin_cpt_settings');
                     echo '<input type="hidden" name="remove" value="'.$option['post_type'].'">';
                     submit_button('Delete', 'delete small', 'submit', false, array(
-                        'onclick' => 'return confirm("Are you sure you want to delete this Custom Post Type? 
-                        The data associated with it will not be deleted.");'
+                        'onclick' => 'return confirm("Are you sure you want to delete this Custom Post Type? \nThe data associated with it will not be deleted.");'
                     ));
                     echo '</form></td></tr>';
 				}
@@ -38,7 +44,7 @@
             ?>
         </div>
 
-        <div id="tab-2" class="tab-pane">
+        <div id="tab-2" class="tab-pane <?php echo $active; ?>">
             
             <form method="POST" action="options.php">
                 <?php
